452c5eece771697fa9d44167a8d57564feb85980
Kyotun <emirpisirici@hotmail.com>
2024-03-17 16:52:58 +0300

    Binary Search Tree data structure. Functions -> find min, find_max, calculate_sum, search a value, in order traversal, add child.

--- bst/example1.py
+++ bst/example1.py
@@ -19,16 +19,74 @@ class BinarySearchTreeNode:
             else:
                 self.right = BinarySearchTreeNode(child)
                 
-    def in_order_traversel(self):
+    def in_order_traversal(self):
         elements = []
-        
         if self.left:
-            elements += self.in_order_traversel()
-        
+            elements += self.left.in_order_traversal()
+            
         elements.append(self.data)
         
         if self.right:
-            elements += self.in_order_traversel()
-        
+            elements += self.right.in_order_traversal()
         return elements
+    
+    def search(self, val):
+        if self.data == val:
+            return True
+        
+        if val < self.data:
+            if self.left:
+                return self.left.search(val=val)
+            else:
+                return False
+        elif val > self.data:
+            if self.right:
+                return self.right.search(val=val)
+            else:
+                return False
+    
+    def find_min(self) -> int:
+        if self.left:
+            return self.left.find_min()
+        return self.data
+    
+    def find_max(self) -> int:
+        if self.right:
+            return self.right.find_max()
+        return self.data
+    
+    def calculate_sum(self) -> int:
+        sum = 0
+        if self.left:
+            sum += self.left.calculate_sum()
+        
+        sum += self.data
+        
+        if self.right:
+            sum += self.right.calculate_sum()
+        
+        return sum
+        
+            
+    
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    
+    for i in range (1,len(elements)):
+        root.add_child(elements[i])
+    
+    return root
+
+if __name__ == '__main__':
+    # countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
+    # country_tree = build_tree(countries)
+
+    # print("UK is in the list? ", country_tree.search("UK"))
+    # print("Sweden is in the list? ", country_tree.search("Sweden"))
+
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
+    print(numbers_tree.find_min())
+    print(numbers_tree.find_max())
+    print(numbers_tree.calculate_sum())
             

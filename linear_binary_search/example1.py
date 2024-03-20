fcaa7e6c62b8cff2e4924e8c442893a8dbcc9b24
Kyotun <emirpisirici@hotmail.com>
2024-03-20 18:35:07 +0300

    Deleted function: binary_search(useless in big data). Added module: util, to measure time of the functions.

--- linear_binary_search/example1.py
+++ linear_binary_search/example1.py
@@ -1,31 +1,20 @@
+from util import time_it
+
+@time_it
 def linear_search(numbers_list, target):
     for number in numbers_list:
         if number == target:
             return numbers_list.index(number)
     raise Exception("Value cannot be found in the given data.")
 
+@time_it
 def linear_search_2(number_list, target):
     for index, element in enumerate(number_list):
         if element == target:
             return index
     raise Exception("Value cannot be found in the given data.")
 
-def binary_search(numbers_list, target):
-    index = 0
-    k = 2
-    while True:
-        if numbers_list[index] < target:
-            index += len(numbers_list)//k
-        elif numbers_list[index] > target:
-            index -= len(numbers_list)//k
-        else:
-            return index
-        
-        if index + (len(numbers_list)//k) == index:
-            break
-        k *= 2
-    raise Exception("Value cannot be found in the given data.")
-
+@time_it
 def binary_search_2(number_list, target):
     left_index = 0
     right_index = len(number_list) - 1
@@ -42,17 +31,12 @@ def binary_search_2(number_list, target):
     raise Exception("Value cannot be found in the given data.")
 
 if __name__ == '__main__':
-    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
-    number_to_find = 67
-
-    index = binary_search(numbers_list, number_to_find)
-    print(f"Number found at index {index} using binary search.")
+    numbers_list = [i for i in range(1000001)]
+    number_to_find = 1000000
 
     index = linear_search(numbers_list, number_to_find)
-    print(f"Number found at index {index} using linear search.")
-
+    index = linear_search_2(numbers_list, number_to_find)
     index = binary_search_2(numbers_list, number_to_find)
-    print(f"Number found at index {index} using binary search 2.")
 
-    index = linear_search_2(number_list=numbers_list, target=number_to_find)
-    print(f"Number found at index {index} using linear search 2.")
+
+

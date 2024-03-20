75cb23444791a5ef8fbc96ccc0da858a32a31d4a
Kyotun <emirpisirici@hotmail.com>
2024-03-20 11:06:24 +0300

    Main is written and functions are tested. No error.

--- graphs/example1.py
+++ graphs/example1.py
@@ -27,4 +27,45 @@ class Graph:
         return paths
     
     def get_shortest_path(self, start, end, path=[]):
-        pass
+        path = path + [start]
+
+        if start == end:
+            return path
+        
+        if start not in self.graph_dict:
+            return None
+        
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path=path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+        
+        return shortest_path
+
+if __name__ == '__main__':
+
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
+    ]
+
+    route_graph = Graph(routes)
+
+    start = "Mumbai"
+    end = "Dubai"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
+
+    start = "Mumbai"
+    end = "Toronto"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))

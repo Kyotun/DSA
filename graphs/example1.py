ffab747758f6b986a70a9d33aed23b6be1248d79
Kyotun <emirpisirici@hotmail.com>
2024-03-20 08:20:53 +0300

    Graph class, completed function: get_paths.

--- graphs/example1.py
+++ graphs/example1.py
@@ -9,4 +9,22 @@ class Graph:
                 self.graph_dict[start] = [end]
         
     def get_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+        
+        if start not in self.graph_dict:
+            return []
+        
+        paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths = self.get_paths(node, end, path=path)
+                for path in new_paths:
+                    paths.append(path)
+                
+        return paths
+    
+    def get_shortest_path(self, start, end, path=[]):
         pass

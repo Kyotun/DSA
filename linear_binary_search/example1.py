d287a43bf0dc8487a419606802e64418c42379c9
Kyotun <emirpisirici@hotmail.com>
2024-03-20 18:59:35 +0300

    Added function: recursive binary search.

--- linear_binary_search/example1.py
+++ linear_binary_search/example1.py
@@ -15,7 +15,7 @@ def linear_search_2(number_list, target):
     raise Exception("Value cannot be found in the given data.")
 
 @time_it
-def binary_search_2(number_list, target):
+def binary_search(number_list, target):
     left_index = 0
     right_index = len(number_list) - 1
     mid_index = (left_index + right_index) // 2
@@ -30,13 +30,37 @@ def binary_search_2(number_list, target):
         mid_index = (left_index + right_index) // 2
     raise Exception("Value cannot be found in the given data.")
 
+@time_it
+def binary_search_recursive(number_list, target, left_index, right_index):
+    if right_index < left_index:
+        raise Exception("Value cannot be found in the given data.")
+    
+    mid_index = (left_index + right_index) // 2
+    if mid_index >= len(number_list) or mid_index < 0:
+        raise Exception("Value cannot be found in the given data.")
+    
+    mid_number = number_list[mid_index]
+
+    if mid_number == target:
+        return mid_index
+
+    if mid_number > target:
+        right_index = mid_index - 1
+    elif mid_number < target:
+        left_index = mid_index + 1
+
+    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
+
+    
+
 if __name__ == '__main__':
     numbers_list = [i for i in range(1000001)]
     number_to_find = 1000000
 
     index = linear_search(numbers_list, number_to_find)
     index = linear_search_2(numbers_list, number_to_find)
-    index = binary_search_2(numbers_list, number_to_find)
+    index = binary_search(numbers_list, number_to_find)
+    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
 
 
 

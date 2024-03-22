4b39c704907b41e2383c09d2d4fd4b1ee94be42e
Kyotun <emirpisirici@hotmail.com>
2024-03-22 07:46:34 +0300

    Hoare and lomuto methods for quick sort algorithms are added.

--- quick-sort/example1.py
+++ quick-sort/example1.py
@@ -1,3 +1,5 @@
+import time
+
 def swap(a, b, arr):
     if a != b:
         temp = arr[a]
@@ -5,7 +7,7 @@ def swap(a, b, arr):
         arr[b] = temp
     return arr
     
-def partition(elements, start, end):
+def partition_hoare(elements, start, end):
     pivot_index = start
     pivot = elements[pivot_index]
 
@@ -19,14 +21,34 @@ def partition(elements, start, end):
     elements = swap(pivot_index, end, elements)
     return end
 
-def quick_sort(elements, start, end):
+def quick_sort_hoare(elements, start, end):
     if start < end:
-        pivot_index = partition(elements, start, end)
-        elements = quick_sort(elements, start, pivot_index-1)
-        elements = quick_sort(elements, pivot_index+1, end)
+        pivot_index = partition_hoare(elements, start, end)
+        elements = quick_sort_hoare(elements, start, pivot_index-1)
+        elements = quick_sort_hoare(elements, pivot_index+1, end)
     return elements
 
-    
+def partition_lomuto(elements, start, end):
+    pivot = elements[end]
+    p_index = start
+
+    for i in range(start, end):
+        if elements[i] <= pivot:
+            swap(i, p_index, elements)
+            p_index += 1
+
+    swap(p_index, end, elements)
+
+    return p_index
+
+def quick_sort_lomuto(elements, start, end):
+    if len(elements)== 1:
+        return
+    if start < end:
+        pi = partition_lomuto(elements, start, end)
+        quick_sort_lomuto(elements, start, pi-1)
+        quick_sort_lomuto(elements, pi+1, end)
+    return elements
 
 if __name__ == '__main__':
     tests = [
@@ -40,6 +62,6 @@ if __name__ == '__main__':
 
     for elements in tests:
         print(f'unsorted array: {elements}')
-        elements = quick_sort(elements, 0, len(elements)-1)
+        elements = quick_sort_lomuto(elements, 0, len(elements)-1)
         print(f'sorted array: {elements}')
         print("")

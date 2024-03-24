f204279e117f94991241d6e64ca2d58932ebf542
Kyotun <emirpisirici@hotmail.com>
2024-03-24 13:20:00 +0100

    Started to work on recursion at a simple level. 2 Function.

---
+++ recursion/example1.py
@@ -0,0 +1,14 @@
+def find_sum(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
+    return sum
+
+def find_sum_rec(n):
+    if n == 1:
+        return n
+    return n + find_sum_rec(n-1)
+
+if __name__ == '__main__':
+    print(find_sum(5))
+    print(find_sum_rec(5))

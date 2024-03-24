157e4b97834f570b4a4d0416b7aa541f51af802e
Kyotun <emirpisirici@hotmail.com>
2024-03-24 13:46:42 +0100

    Added functionality to calculate fibonacci.

--- recursion/example1.py
+++ recursion/example1.py
@@ -9,6 +9,15 @@ def find_sum_rec(n):
         return n
     return n + find_sum_rec(n-1)
 
+# 0, 1, 1, 2, 3, 5, 8, 13, 21
+# 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+    
+
 if __name__ == '__main__':
     print(find_sum(5))
     print(find_sum_rec(5))
+    print(fibonacci(n=7))

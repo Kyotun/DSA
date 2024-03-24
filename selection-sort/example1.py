0ad040a3529ebdab29d466434216f5b48329076b
Kyotun <emirpisirici@hotmail.com>
2024-03-24 12:20:14 +0100

    Added test cases to first algorithm and tried them.

--- selection-sort/example1.py
+++ selection-sort/example1.py
@@ -14,6 +14,14 @@ def selection_sort(array):
         array[index] = temp
 
 if __name__ == '__main__':
-    elements = [78,12,15,8,61,53,23,27]
-    selection_sort(elements)
-    print(elements)
+    tests = [
+        [89, 78, 61, 53, 23, 21, 17, 12, 9, 7, 6, 2, 1],
+        [],
+        [1,5,8,9],
+        [234,3,1,56,34,12,9,12,1300],
+        [78, 12, 15, 8, 61, 53, 23, 27],
+        [5]
+    ]
+    for elements in tests:
+        selection_sort(elements)
+        print(elements)

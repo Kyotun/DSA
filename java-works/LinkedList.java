ee1342781f4546c12caf1e2e88d8d6a33eb49310
Kyotun <emirpisirici@hotmail.com>
2024-03-26 11:59:50 +0100

    Linked List class: Added function -> add.

--- java-works/LinkedList.java
+++ java-works/LinkedList.java
@@ -1,2 +1,30 @@
 public class LinkedList {
+    ListNode head = null;
+
+    public void add(int value, int index) {
+        if (head == null) {
+            this.head = new ListNode();
+            this.head.value = value;
+        } else if (index == 0) {
+            ListNode ml = new ListNode();
+            ml.value = value;
+            ml.next = this.head;
+            this.head = ml;
+        } else {
+            ListNode temp = this.head;
+            int count = 1;
+            while(count < index && temp != null) {
+                temp = temp.next;
+                count++;
+            }
+            if(temp != null){
+                ListNode ml = new ListNode();
+                ml.value = value;
+                ml.next = temp.next;
+                temp.next = ml;
+            } else {
+                System.out.println("Index is out of range.");
+            }
+        }
+    }
 }

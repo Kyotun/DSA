b73f9f118061fc48739de9fbe4610fe0660c33c3
Kyotun <emirpisirici@hotmail.com>
2024-03-26 13:23:45 +0100

    New class: Map, added two function -> add, get.

---
+++ java-works/Map.java
@@ -0,0 +1,11 @@
+class Map{
+    
+    String[] values = new String[10000];
+
+    public void add(int key, String value){
+        values[key] = values;
+    }
+    public String get(int key){
+        return values[key];
+    }
+}

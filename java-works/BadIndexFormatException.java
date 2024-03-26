693bb0487419c357b3ec3ca5fda89d444bc7e5c8
Kyotun <emirpisirici@hotmail.com>
2024-03-26 12:13:52 +0100

    BadIndexFormatException class: Added attribute, constructor and set&get methods.

--- java-works/BadIndexFormatException.java
+++ java-works/BadIndexFormatException.java
@@ -1,2 +1,14 @@
-public class BadIndexFormatException {
+public class BadIndexFormatException extends Exception{
+    private String message;
+
+    public BadIndexFormatException(String msg){
+        super(msg);
+    }
+
+    public void setMessage(String msg){
+        this.message = msg;
+    }
+    public String getMessage(){
+        return this.message;
+    }
 }

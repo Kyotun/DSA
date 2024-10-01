532f21f7d34871e1c62b7a2f6fa46ac884530b22
Kyotun <emirpisirici@hotmail.com>
2024-10-01 23:53:08 +0200

    Merge-sort: Added the type 1

---
+++ python-works/merge-sort/merge-sort1.py
@@ -0,0 +1,53 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return
+
+    mid = len(array) // 2
+    right = array[mid:]
+    left = array[:mid]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge_two_sorted_lists(left, right, array)
+
+
+def merge_two_sorted_lists(left_child, right_child, parent_array):
+    len_left = len(left_child)
+    len_right = len(right_child)
+
+    i = j = k = 0
+
+    while i < len_left and j < len_right:
+        if left_child[i] <= right_child[j]:
+            parent_array[k] = left_child[i]
+            i += 1
+        else:
+            parent_array[k] = right_child[j]
+            j += 1
+        k += 1
+
+    while i < len_left:
+        parent_array[k] = left_child[i]
+        i += 1
+        k += 1
+
+    while j < len_right:
+        parent_array[k] = right_child[j]
+        j += 1
+        k += 1
+
+    print(parent_array)
+
+
+if __name__ == '__main__':
+    test_cases = [
+        [10, 3, 15, 7, 8, 23, 98, 29],
+        [],
+        [3],
+        [9, 8, 7, 2],
+        [1, 2, 3, 4, 5]
+    ]
+
+    my_array = [3, 5, 2, 6, 1, 7, 4]
+    print(len(my_array)//2)
